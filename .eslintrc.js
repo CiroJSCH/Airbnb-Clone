c46e6e9e59eabc749707c9f419a9797702baa163
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-05 19:38:55 -0300

    feat: auto sort imports eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,24 @@ module.exports = {
 		"semi": [2, "always"],
 		"indent": [2, "tab"],
 		"comma-dangle": [2, "always-multiline"],
+		"import/order": [
+			"error",
+			{
+				"groups": ["builtin", "external", "internal"],
+				"pathGroups": [
+					{
+						"pattern": "react",
+						"group": "external",
+						"position": "before",
+					},
+				],
+				"pathGroupsExcludedImportTypes": ["react"],
+				"newlines-between": "always",
+				"alphabetize": {
+					"order": "desc",
+					"caseInsensitive": true,
+				},
+			},
+		],
 	},
 };
