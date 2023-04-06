3deab7a5113b4ff156a48f691d52393ecd1cabca
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-06 15:40:15 -0300

    ci: single quotes rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,44 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
+	'env': {
+		'browser': true,
+		'es2021': true,
 	},
-	"extends": [
-		"plugin:@typescript-eslint/recommended",
-		"next",
-		"next/core-web-vitals",
-		"prettier",
+	'extends': [
+		'plugin:@typescript-eslint/recommended',
+		'next',
+		'next/core-web-vitals',
+		'prettier',
 	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaFeatures": {
-			"jsx": true,
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true,
 		},
-		"ecmaVersion": 12,
-		"sourceType": "module",
+		'ecmaVersion': 12,
+		'sourceType': 'module',
 	},
-	"plugins": ["@typescript-eslint"],
-	"rules": {
-		"semi": [2, "always"],
-		"indent": [2, "tab"],
-		"comma-dangle": [2, "always-multiline"],
-		"import/order": [
-			"error",
+	'plugins': ['@typescript-eslint'],
+	'rules': {
+		'semi': [2, 'always'],
+		'indent': [2, 'tab'],
+		'comma-dangle': [2, 'always-multiline'],
+		'quotes': [2, 'single', { 'avoidEscape': true }],
+		'import/order': [
+			'error',
 			{
-				"groups": ["builtin", "external", "internal"],
-				"pathGroups": [
+				'groups': ['builtin', 'external', 'internal'],
+				'pathGroups': [
 					{
-						"pattern": "react",
-						"group": "external",
-						"position": "before",
+						'pattern': 'react',
+						'group': 'external',
+						'position': 'before',
 					},
 				],
-				"pathGroupsExcludedImportTypes": ["react"],
-				"newlines-between": "always",
-				"alphabetize": {
-					"order": "desc",
-					"caseInsensitive": true,
+				'pathGroupsExcludedImportTypes': ['react'],
+				'newlines-between': 'always',
+				'alphabetize': {
+					'order': 'desc',
+					'caseInsensitive': true,
 				},
 			},
 		],
