e128a16470328bea8b4fca2cfd45ed59121c0e10
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-07 15:39:53 -0300

    feat: current user props

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import { SafeUser } from '@/types';
 import Container from '@/components/shared/Container';
 
 import UserMenu from './UserMenu';
 import Search from './Search';
 import Logo from './Logo';
 
-const Navbar = () => {
+interface INavbarProps {
+	currentUser?: SafeUser | null;
+}
+
+const Navbar = ({currentUser}: INavbarProps) => {
+	console.log(currentUser);
 	return (
 		<nav className='fixed w-full bg-white z-10 shadow-sm'>
 			<div className='py-4 border-b'>
@@ -14,7 +20,7 @@ const Navbar = () => {
 					<div className='flex items-center justify-between gap-3 md:gap-0'>
 						<Logo />
 						<Search/>
-						<UserMenu />
+						<UserMenu currentUser={currentUser}/>
 					</div>
 				</Container>
 			</div>
