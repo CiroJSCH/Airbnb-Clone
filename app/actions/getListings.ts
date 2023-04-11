684c0a91cc451483ea5f753615dd5a166e039c8b
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-11 09:46:11 -0300

    feat: get all listings

---
+++ app/actions/getListings.ts
@@ -0,0 +1,20 @@
+import prisma from '@/libs/prismadb';
+
+const getListings = async () => {
+	try {
+		const listings = await prisma.listing.findMany({
+			orderBy: {
+				createdAt: 'desc',
+			},
+		});
+		const safeListings = listings.map(listing => ({
+			...listing,
+			createdAt: listing.createdAt.toISOString(),
+		}));
+		return safeListings;
+	} catch (error: any) {
+		throw new Error(error);
+	}
+};
+
+export default getListings;
