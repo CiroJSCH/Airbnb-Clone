bb59d397ea521de32682bb4a1d162fcee3e13a41
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-14 10:17:44 -0300

    feat: dynamic search labels

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,25 +1,64 @@
 'use client';
+import { useMemo } from 'react';
+
 import { BiSearch } from 'react-icons/bi';
+import { useSearchParams } from 'next/navigation';
+import { differenceInDays } from 'date-fns';
 
 import useSearchModal from '@/hooks/useSearchModal';
+import useCountries from '@/hooks/useCountries';
 
 const Search = () => {
 	const searchModal = useSearchModal();
+	const params = useSearchParams();
+	const { getByValue } = useCountries();
+
+	const locationValue = params?.get('locationValue');
+	const startDate = params?.get('startDate');
+	const endDate = params?.get('endDate');
+	const guestCount = params?.get('guestCount');
+
+	const locationLabel = useMemo(() => {
+		if (locationValue) return getByValue(locationValue as string)?.label;
+
+		return 'Anywhere';
+	}, [getByValue, locationValue]);
+
+	const durationLabel = useMemo(() => {
+		if (startDate && endDate) {
+			const start = new Date(startDate as string);
+			const end = new Date(endDate as string);
+
+			let diff = differenceInDays(end, start);
+
+			if (diff === 0) diff = 1;
+
+			return `${diff} days`;
+		}
+
+		return 'Any week';
+	}, [endDate, startDate]);
+
+	const guestLabel = useMemo(() => {
+		if (guestCount) return `${guestCount} guests`;
+
+		return 'Add Guests';
+	}, [guestCount]);
+
 	return (
-		<div onClick={searchModal.onOpen} className='border w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
+		<div
+			onClick={searchModal.onOpen}
+			className="w-full cursor-pointer rounded-full border py-2 shadow-sm transition hover:shadow-md md:w-auto"
+		>
 			<div className="flex flex-row items-center justify-between">
-				<div className="text-sm font-semibold px-6">
-          Anywhere
+				<div className="px-6 text-sm font-semibold">{locationLabel}</div>
+				<div className="hidden flex-1 border-x px-6 text-center text-sm font-semibold sm:block">
+					{durationLabel}
 				</div>
-				<div className="hidden sm:block text-sm font-semibold px-6 border-x flex-1 text-center">
-          Any week
-				</div>
-				<div className="text-sm pl-6 pr-2 text-gray-600 flex items-center gap-3">
-					<div className="hidden sm:block">
-            Add Guests
-					</div>
-					<div className="p-2 bg-rose-500 rounded-full text-white">
-						<BiSearch size="18"/>
+				<div className="flex items-center gap-3 pl-6 pr-2 text-sm text-gray-600">
+					<div className="hidden sm:block">{guestLabel}</div>
+					<div className="rounded-full bg-rose-500 p-2 text-white">
+						<BiSearch size="18" />
 					</div>
 				</div>
 			</div>
