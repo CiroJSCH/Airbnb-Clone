f495e71c3cf47b8638a27a3c89c7e8da483b89c5
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-14 09:50:33 -0300

    feat: search filters when get listings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,16 +1,77 @@
 import prisma from '@/libs/prismadb';
 
 export interface IListingsParams {
-	userId?: string
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 const getListings = async (params: IListingsParams) => {
 	try {
-		const { userId } = params;
+		const {
+			userId,
+			roomCount,
+			bathroomCount,
+			category,
+			endDate,
+			guestCount,
+			locationValue,
+			startDate,
+		} = params;
 
 		const query: any = {};
 
 		if (userId) query.userId = userId;
+
+
+		if (category) query.category = category;
+
+		if (roomCount) {
+			query.roomCount = {
+				gte: +roomCount, // mayor o igual al número pasado
+			};
+		}
+
+		if (guestCount) {
+			query.guestCount = {
+				gte: +guestCount,
+			};
+		}
+
+		if (bathroomCount) {
+			query.bathroomCount = {
+				gte: +bathroomCount,
+			};
+		}
+
+		if (locationValue) query.locationValue = locationValue;
+
+		if (startDate && endDate) {
+			query.NOT = {
+				reservations: {
+					some: {
+						OR: [
+							{
+								endDate: { gte: startDate },
+								startDate: { lte: startDate },
+							},
+							{
+								startDate: { lte: endDate },
+								endDate: { gte: endDate },
+							},
+						],
+					},
+				},
+			};
+		}
+
+
+
 
 		const listings = await prisma.listing.findMany({
 			where: query,
@@ -18,7 +79,7 @@ const getListings = async (params: IListingsParams) => {
 				createdAt: 'desc',
 			},
 		});
-		const safeListings = listings.map(listing => ({
+		const safeListings = listings.map((listing) => ({
 			...listing,
 			createdAt: listing.createdAt.toISOString(),
 		}));
