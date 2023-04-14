1f375ab8c9616d592ea3a3708b2eab3f0aedd71c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-14 09:50:03 -0300

    fix: fixed listing creation error

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -3,13 +3,17 @@ import { NextResponse } from 'next/server';
 import prisma from '@/libs/prismadb';
 import { getCurrentUser } from '@/actions/getCurrentUser';
 
-export const POST = async (request: Request) => {
+export async function POST(
+	request: Request, 
+) {
 	const currentUser = await getCurrentUser();
 
-	if (!currentUser) return NextResponse.error();
+	if (!currentUser) {
+		return NextResponse.error();
+	}
 
 	const body = await request.json();
-	const {
+	const { 
 		title,
 		description,
 		imageSrc,
@@ -37,4 +41,4 @@ export const POST = async (request: Request) => {
 	});
 
 	return NextResponse.json(listing);
-};
+}
